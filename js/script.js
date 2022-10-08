ba9ade0045962b6ac4f0eee72a42d2e4926a6c10
Libertifer <99355707+Libertifer@users.noreply.github.com>
2022-10-08 01:55:41 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,3 +22,9 @@ document.getElementById('initius').innerHTML = textus['hispanus'][0];
 document.getElementById('qui-sumus').innerHTML = textus['hispanus'][1];
 document.getElementById('contactare-nos').innerHTML = textus['hispanus'][2];
 document.getElementById('lingua').innerHTML = textus['hispanus'][3];
+
+function motioTituli() {
+  titulus = titulus.substring(1, titulus.length) + titulus.substring(0, 1);
+  document.title = titulus;
+  setTimeout("motioTituli()", 60);
+}
