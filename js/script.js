312ad09c9efb2661539ce046bbf19280b1827f96
Libertifer <99355707+Libertifer@users.noreply.github.com>
2022-10-08 00:52:20 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var textus = {
+  'hispanus': [
+    'Inicio',
+    'Quiénes Somos',
+    'Contacto',
+    'Idioma',
+  ],
+  'anglicus': [
+    'Home',
+    'About us',
+    'Contact',
+    'Language'
+  ]
+};
+
+var linguae = [
+  'Español',
+  'English'
+]
+
+document.getElementById('initius').innerHTML = textus['hispanus'][0];
+document.getElementById('qui-sumus').innerHTML = textus['hispanus'][1];
+document.getElementById('contactare-nos').innerHTML = textus['hispanus'][2];
+document.getElementById('lingua').innerHTML = textus['hispanus'][3];
