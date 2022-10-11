9be600d975efa4e195a17b2f3db7092364f4ab13
Libertifer <99355707+Libertifer@users.noreply.github.com>
2022-10-11 20:33:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ document.getElementById('initius').innerHTML = textus[linguaElecta][0][0];
 document.getElementById('qui-sumus').innerHTML = textus[linguaElecta][0][1];
 document.getElementById('contactare-nos').innerHTML = textus[linguaElecta][0][2];
 document.getElementById('lingua').innerHTML = textus[linguaElecta][0][3];
-document.getElementById('ludi').innerHTML = textus[linguaElecta][0][4];
+document.getElementById('ludi').innerHTML = `<h2>${textus[linguaElecta][0][4]}</h2>`;
 
 for (let ludus in ludi) {
   document.getElementById('principalis').innerHTML += `<fieldset id="ludus-${ludus}"><legend>${ludi[ludus]}</legend><p>${textus[linguaElecta][1][ludus]}</p></fieldset>`;
