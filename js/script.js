f21785bc463866d8bbe84087e5b431a188ee7b0d
Libertifer <99355707+Libertifer@users.noreply.github.com>
2022-10-10 21:36:05 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 var textus = {
-  'hispanus': [
-    'INICIO',
-    'QUIÉNES SOMOS',
-    'CONTACTO',
-    'IDIOMA',
+  'hispana': [
+    [
+      'INICIO',
+      'QUIÉNES SOMOS',
+      'CONTACTO',
+      'IDIOMA',
+      'Juegos'
+    ],[
+      '¿Alguna vez has jugado a juegos Tower Defense? En plan ¿que debes construir torres para detener a un ejército maligno que quiere destruir tu base? Pues, ahora tú vas a construir las torres para ayudar al ejército maligno (o bueno, ya que están de tu lado) a destruir la base.'
+    ],[
+      'Desarrollador web en stack LAMP, con manejo de frameworks como Bootstrap, Tailwind, React y Vue.js.',
+      'Diseñador gráfico de sprites y backgrounds en Paint Tool SAI y Aseprite.'
+    ]
   ],
-  'anglicus': [
-    'HOME',
-    'ABOUT US',
-    'CONTACT',
-    'LANGUAGE'
+  'anglica': [
+    [
+      'HOME',
+      'ABOUT US',
+      'CONTACT',
+      'LANGUAGE',
+      'Games'
+    ],[
+      'Have you ever played Tower Defense games? Like you gotta build towers to stop some evil army from destroying your base? Well, now you will build the towers to help the evil (or good, since they\'re on your side) army to destroy the base.'
+    ],[
+      'LAMP web developer, skilled with frameworks like Bootstrap, Tailwind, React and Vue.js.',
+      'Graphic designer of sprites and backgrounds with Paint Tool SAI and Aseprite.'
+    ]
   ]
 };
 
@@ -18,10 +34,27 @@ var linguae = [
   'ENGLISH'
 ]
 
-document.getElementById('initius').innerHTML = textus['hispanus'][0];
-document.getElementById('qui-sumus').innerHTML = textus['hispanus'][1];
-document.getElementById('contactare-nos').innerHTML = textus['hispanus'][2];
-document.getElementById('lingua').innerHTML = textus['hispanus'][3];
+var comites = [
+  'Libertifer',
+  'Ahkin Arts'
+]
+
+var ludi = [
+  'Nokia Tower Offense'  
+]
+
+var linguaElecta = 'hispana';
+
+document.getElementById('initius').innerHTML = textus[linguaElecta][0][0];
+document.getElementById('qui-sumus').innerHTML = textus[linguaElecta][0][1];
+document.getElementById('contactare-nos').innerHTML = textus[linguaElecta][0][2];
+document.getElementById('lingua').innerHTML = textus[linguaElecta][0][3];
+
+i = 0;
+for ludus in ludi {
+  document.getElementById('principalis').innerHTML += `<fieldset><legend>${ludus}</legend><p>${textus[linguaElecta][1][i]}</p></fieldset>`;
+  i++;
+}
 
 var titulus = document.title;
 function motioTituli() {
