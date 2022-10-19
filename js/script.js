8239fb61bfccfcd1cf9540da6439015c35a03c61
Libertifer <99355707+Libertifer@users.noreply.github.com>
2022-10-19 15:26:30 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 
 var linguaElecta = 'hispana';
 
-for (let ludus in ludi) {
-  document.getElementById('principalis').innerHTML += `<fieldset id="ludus-${ludus}"><legend>${ludi[ludus][0]}</legend><div class="descriptio"><img src="img/${ludi[ludus][1]}"/><div><p>${textus[linguaElecta][1][ludus]}</p><input type="button" value="Más información" onclick="window.open('${ludi[ludus][2]}')"></div></div></fieldset>`;
+async function textusLegere(archivum) {
+  let petitum = await fetch(archivum);
+  let catenaDatorum = await petitum.text();
+  let data = JSON.parse(catenaDatorum);
+  return data;
+}
+
+function indexScribere(obiectum) {
+  document.getElementById('initius').innerHTML = obiectum['textus'][linguaElecta]['index']['initius'];
+  document.getElementById('qui-sumus').innerHTML = obiectum['textus'][linguaElecta]['index']['qui-sumus'];
+  document.getElementById('communica-nos').innerHTML = obiectum['textus'][linguaElecta]['index']['communica-nos'];
+  document.getElementById('lingua').innerHTML = obiectum['textus'][linguaElecta]['index']['lingua'];
+}
+
+function paginaScribere(obiectum) {
+  document.getElementById('ludi').innerHTML = obiectum['textus'][linguaElecta]['sectiones']['ludi'];
+}
+
+var obiectum = textusLegere("./json/db.json");
+indexScribere(obiectum);
+paginaScribere(obiectum);
+
+for (let ludus in obiectum['ludi']) {
+  document.getElementById('principalis').innerHTML += `<fieldset id="ludus-${ludus}"><legend>${obiectum['ludi'][ludus]['nomen']}</legend><div class="descriptio"><img src="img/${obiectum['ludi'][ludus]['imago']}"/><div><p>${obiectum['ludi'][ludus]['descriptio'][linguaElecta]}</p><input type="button" value="Más información" onclick="window.open('${obiectum['ludi'][ludus]['vinculum']}')"></div></div></fieldset>`;
 }
 
 var titulus = document.title;
@@ -11,18 +33,3 @@ function motioTituli() {
   document.title = titulus;
   setTimeout("motioTituli()", 500);
 }
-
-async function textusLegere(archivum) {
-  let petitum = await fetch(archivum);
-  let catenaDatorum = await petitum.text();
-  let data = JSON.parse(catenaDatorum);
-  console.log(data['textus']);
-  console.log(data);
-  document.getElementById('initius').innerHTML = data['textus'][linguaElecta]['index']['initius'];
-  document.getElementById('qui-sumus').innerHTML = data['textus'][linguaElecta]['index']['qui-sumus'];
-  document.getElementById('communica-nos').innerHTML = data['textus'][linguaElecta]['index']['communica-nos'];
-  document.getElementById('lingua').innerHTML = data['textus'][linguaElecta]['index']['lingua'];
-  document.getElementById('ludi').innerHTML = data['textus'][linguaElecta]['sectiones']['ludi'];
-}
-
-textusLegere("./json/db.json");
